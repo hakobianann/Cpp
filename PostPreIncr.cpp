b860fb588be937e2c6f17ba6269c6dcd72e905a6
Ann <40519330+naibokaHnnA@users.noreply.github.com>
2019-02-21 23:11:29 +0400

    Update PostPreIncr.cpp

--- PostPreIncr.cpp
+++ PostPreIncr.cpp
@@ -9,22 +9,29 @@ class Number {
 
 public:
 	int x;
+	Number(int y) :x(y) {  };
 	Number& operator++();
 	Number operator++(int);
+	void operator=(Number&);
+
 };
 Number& Number::operator++() {
 	return *this;
   }
-
+																	
+void Number :: operator=(Number &t) {
+	x = t.x;
+}
 Number Number::operator++(int) {
 	Number temp = *this;
 	++(*this);
 	return temp;
+
 }
 int main()
 {
-	Number num ;
-	num.x = 7;
+	Number num(7),n(8) ;
+	num = n;
 	int y=num.x++;
 	int z = ++num.x;
 	cout << y << num.x << z;
